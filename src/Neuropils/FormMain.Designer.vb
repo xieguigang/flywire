8f22b2dd058e5bde3774e628d890e2a629663cf7
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-09-24 14:42:09 +0800

    add ui components

--- src/Neuropils/FormMain.Designer.vb
+++ src/Neuropils/FormMain.Designer.vb
@@ -22,9 +22,61 @@ Partial Class FormMain
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        components = New ComponentModel.Container()
+        VS2015LightTheme1 = New ThemeVS2015.VS2015LightTheme()
+        DockPanel1 = New Microsoft.VisualStudio.WinForms.Docking.DockPanel()
+        VisualStudioToolStripExtender1 = New Microsoft.VisualStudio.WinForms.Docking.VisualStudioToolStripExtender(components)
+        Ribbon1 = New RibbonLib.Ribbon()
+        StatusStrip1 = New StatusStrip()
+        SuspendLayout()
+        ' 
+        ' DockPanel1
+        ' 
+        DockPanel1.Dock = DockStyle.Fill
+        DockPanel1.Location = New Point(0, 116)
+        DockPanel1.Name = "DockPanel1"
+        DockPanel1.Size = New Size(1428, 705)
+        DockPanel1.TabIndex = 0
+        ' 
+        ' VisualStudioToolStripExtender1
+        ' 
+        VisualStudioToolStripExtender1.DefaultRenderer = Nothing
+        ' 
+        ' Ribbon1
+        ' 
+        Ribbon1.Location = New Point(0, 0)
+        Ribbon1.Name = "Ribbon1"
+        Ribbon1.ResourceIdentifier = Nothing
+        Ribbon1.ResourceName = Nothing
+        Ribbon1.ShortcutTableResourceName = Nothing
+        Ribbon1.Size = New Size(1428, 116)
+        Ribbon1.TabIndex = 1
+        ' 
+        ' StatusStrip1
+        ' 
+        StatusStrip1.Location = New Point(0, 821)
+        StatusStrip1.Name = "StatusStrip1"
+        StatusStrip1.Size = New Size(1428, 22)
+        StatusStrip1.TabIndex = 2
+        StatusStrip1.Text = "StatusStrip1"
+        ' 
+        ' FormMain
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(1428, 843)
+        Controls.Add(DockPanel1)
+        Controls.Add(StatusStrip1)
+        Controls.Add(Ribbon1)
+        Name = "FormMain"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
     End Sub
+
+    Friend WithEvents VS2015LightTheme1 As ThemeVS2015.VS2015LightTheme
+    Friend WithEvents DockPanel1 As Microsoft.VisualStudio.WinForms.Docking.DockPanel
+    Friend WithEvents VisualStudioToolStripExtender1 As Microsoft.VisualStudio.WinForms.Docking.VisualStudioToolStripExtender
+    Friend WithEvents Ribbon1 As RibbonLib.Ribbon
+    Friend WithEvents StatusStrip1 As StatusStrip
 End Class
