d214b9c4dddf39345e5d7bd81ae15f07b277471f
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-09-24 14:53:29 +0800

    add controls

--- src/Neuropils/FormMain.Designer.vb
+++ src/Neuropils/FormMain.Designer.vb
@@ -28,6 +28,10 @@ Partial Class FormMain
         VisualStudioToolStripExtender1 = New Microsoft.VisualStudio.WinForms.Docking.VisualStudioToolStripExtender(components)
         Ribbon1 = New Global.RibbonLib.Ribbon()
         StatusStrip1 = New StatusStrip()
+        ToolStripStatusLabel1 = New ToolStripStatusLabel()
+        ToolStripProgressBar1 = New ToolStripProgressBar()
+        ToolStripStatusLabel2 = New ToolStripStatusLabel()
+        StatusStrip1.SuspendLayout()
         SuspendLayout()
         ' 
         ' DockPanel1
@@ -54,11 +58,29 @@ Partial Class FormMain
         ' 
         ' StatusStrip1
         ' 
+        StatusStrip1.Items.AddRange(New ToolStripItem() {ToolStripStatusLabel1, ToolStripStatusLabel2, ToolStripProgressBar1})
         StatusStrip1.Location = New Point(0, 821)
         StatusStrip1.Name = "StatusStrip1"
         StatusStrip1.Size = New Size(1428, 22)
         StatusStrip1.TabIndex = 2
         StatusStrip1.Text = "StatusStrip1"
+        ' 
+        ' ToolStripStatusLabel1
+        ' 
+        ToolStripStatusLabel1.Name = "ToolStripStatusLabel1"
+        ToolStripStatusLabel1.Size = New Size(42, 17)
+        ToolStripStatusLabel1.Text = "Ready!"
+        ' 
+        ' ToolStripProgressBar1
+        ' 
+        ToolStripProgressBar1.Name = "ToolStripProgressBar1"
+        ToolStripProgressBar1.Size = New Size(100, 16)
+        ' 
+        ' ToolStripStatusLabel2
+        ' 
+        ToolStripStatusLabel2.Name = "ToolStripStatusLabel2"
+        ToolStripStatusLabel2.Size = New Size(1269, 17)
+        ToolStripStatusLabel2.Spring = True
         ' 
         ' FormMain
         ' 
@@ -70,6 +92,8 @@ Partial Class FormMain
         Controls.Add(Ribbon1)
         Name = "FormMain"
         Text = "Form1"
+        StatusStrip1.ResumeLayout(False)
+        StatusStrip1.PerformLayout()
         ResumeLayout(False)
         PerformLayout()
     End Sub
@@ -79,4 +103,7 @@ Partial Class FormMain
     Friend WithEvents VisualStudioToolStripExtender1 As Microsoft.VisualStudio.WinForms.Docking.VisualStudioToolStripExtender
     Friend WithEvents Ribbon1 As Global.RibbonLib.Ribbon
     Friend WithEvents StatusStrip1 As StatusStrip
+    Friend WithEvents ToolStripStatusLabel1 As ToolStripStatusLabel
+    Friend WithEvents ToolStripStatusLabel2 As ToolStripStatusLabel
+    Friend WithEvents ToolStripProgressBar1 As ToolStripProgressBar
 End Class
--- src/Neuropils/FormMain.vb
+++ src/Neuropils/FormMain.vb
@@ -1,3 +1,76 @@
-﻿Public Class FormMain
+﻿Imports Galaxy.Workbench
 
+Public Class FormMain : Implements AppHost
+
+    Public ReadOnly Property ActiveDocument As Form Implements AppHost.ActiveDocument
+        Get
+            Return DockPanel1.ActiveDocument
+        End Get
+    End Property
+
+    Private ReadOnly Property AppHost_ClientRectangle As Rectangle Implements AppHost.ClientRectangle
+        Get
+            Return New Rectangle(Location, Size)
+        End Get
+    End Property
+
+    Public Event ResizeForm As AppHost.ResizeFormEventHandler Implements AppHost.ResizeForm
+    Public Event CloseWorkbench As AppHost.CloseWorkbenchEventHandler Implements AppHost.CloseWorkbench
+
+    Public Sub SetWorkbenchVisible(visible As Boolean) Implements AppHost.SetWorkbenchVisible
+        Me.Visible = visible
+    End Sub
+
+    Public Sub SetWindowState(stat As FormWindowState) Implements AppHost.SetWindowState
+        Me.WindowState = stat
+    End Sub
+
+    Public Sub SetTitle(title As String) Implements AppHost.SetTitle
+        Me.Text = "Neuropils - " & title
+    End Sub
+
+    Public Sub StatusMessage(msg As String, Optional icon As Image = Nothing) Implements AppHost.StatusMessage
+        ToolStripStatusLabel1.Text = msg
+        ToolStripStatusLabel1.Image = icon
+    End Sub
+
+    Public Sub Warning(msg As String) Implements AppHost.Warning
+        Call StatusMessage(msg, Icons8.Warning)
+    End Sub
+
+    Public Sub LogText(text As String) Implements AppHost.LogText
+        Call CommonRuntime.GetOutputWindow.AppendLine(text)
+    End Sub
+
+    Public Sub ShowProperties(obj As Object) Implements AppHost.ShowProperties
+        Call CommonRuntime.GetPropertyWindow.SetObject(obj)
+    End Sub
+
+    Private Sub FormMain_ResizeEnd(sender As Object, e As EventArgs) Handles Me.ResizeEnd
+        RaiseEvent ResizeForm(Location, Size)
+    End Sub
+
+    Private Sub FormMain_FormClosing(sender As Object, e As FormClosingEventArgs) Handles Me.FormClosing
+        RaiseEvent CloseWorkbench(e)
+    End Sub
+
+    Public Function GetDesktopLocation() As Point Implements AppHost.GetDesktopLocation
+        Return Location
+    End Function
+
+    Public Function GetClientSize() As Size Implements AppHost.GetClientSize
+        Return Size
+    End Function
+
+    Public Function GetDocuments() As IEnumerable(Of Form) Implements AppHost.GetDocuments
+        Return DockPanel1.Documents.OfType(Of Form)
+    End Function
+
+    Public Function GetDockPanel() As Control Implements AppHost.GetDockPanel
+        Return DockPanel1
+    End Function
+
+    Public Function GetWindowState() As FormWindowState Implements AppHost.GetWindowState
+        Return WindowState
+    End Function
 End Class
